eb87ec0dcc76718becfc4bad918022de1be3ce84
Maksim Popov <m.popov@bpcbt.com>
2024-11-02 12:14:21 +0300

    Sandbox Landing

---
+++ README.md
@@ -0,0 +1,11 @@
+# BelkaPay Landing
+
+Сделано на [Astro](https://astro.build/)
+
+## Разработка
+
+```
+npm run dev
+```
+
+https://localhost:4321/
---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import localhostCerts from 'vite-plugin-localhost-certs';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  outDir: './build',
+  build: {
+    format: 'file',
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/variables.scss";`,
+        },
+      },
+    },
+    plugins: [localhostCerts()],
+  },
+});
